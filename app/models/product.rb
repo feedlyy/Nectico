0bf38c60c7a92550ba8c9d8282064accc42c6c57
feedlyy <muhammadnurfadlifadli@gmail.com>
2020-06-26 23:58:45 +0700

    Update product model
    added polymorphic assosiation to labels

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+  has_many :labels, dependent: :delete_all
   validates :name, presence: true
   validates :price, numericality: true, presence: true
   validates :store_id, presence: true, numericality: true
