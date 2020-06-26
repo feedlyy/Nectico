6b9aff7381f3ed7cf12c6adf57a8eb77abe68156
feedlyy <muhammadnurfadlifadli@gmail.com>
2020-06-27 00:30:19 +0700

    Update foreign key constraint

--- db/migrate/20200626113853_add_products_reference_to_labels.rb
+++ db/migrate/20200626113853_add_products_reference_to_labels.rb
@@ -1,5 +1,5 @@
 class AddProductsReferenceToLabels < ActiveRecord::Migration[6.0]
   def change
-    add_reference :labels, :products, null: false, foreign_key: true
+    add_reference :labels, :product, null: false, foreign_key: true
   end
 end
